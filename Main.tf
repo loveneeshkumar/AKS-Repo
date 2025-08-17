4a12ae30ff18f676d91a1f05ad9463bfb13cc510
Loveneesh Kumar <er.loveneeshkumar@gmail.com>
2025-08-17 14:07:26 +0530

    akscode

---
+++ Main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "Loveaks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "LoveneeshAKSCluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myakscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  #kubernetes_version = "1.27.103" # Update if needed
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
---
+++ Provider.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "96a3eaff-d4cf-4222-9fe7-1622ab46f13e"
+}
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+
+
